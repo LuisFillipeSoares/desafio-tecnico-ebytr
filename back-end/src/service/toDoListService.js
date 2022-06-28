c63b61ea27a5e92b8e58d399b28faac4732b34db
LuisSoares <jobluisfillipe@gmail.com>
2022-06-28 20:10:34 -0300

    [Service] Adicionando camada de service para toDoList

---
+++ back-end/src/service/toDoListService.js
@@ -0,0 +1,51 @@
+const toDoListModel = require('../model/toDoListModel');
+const error = require('../utils/error');
+
+const getAll = async () => {
+  const result = await toDoListModel.getAll();
+  return result;
+};
+
+const getById = async (id) => {
+  const [result] = await toDoListModel.getById(id);
+  if (!result) throw error.throwError(404, 'A tarefa não existe');
+  return result;
+};
+
+const getByName = async (name) => {
+  const [result] = await toDoListModel.getByname(name);
+  if (!result) throw error.throwError(404, 'Tarefa não encontrada');
+  return result;
+};
+
+const duplicate = async (name) => {
+  const [results] = await toDoListModel.getByname(name);
+  if (!results) return false;
+  return true;
+};
+
+const newTarefa = async (name) => {
+  if (await duplicate(name)) throw error.throwError(409, 'A tarefa já existe');
+  const product = await toDoListModel.newTarefa(name);
+  return product;
+};
+
+const updateTarefa = async (id, name) => {
+  const product = await toDoListModel.updateTarefa(id, name);
+  return product;
+};
+
+const deleteTarefa = async (id) => {
+  const product = await toDoListModel.deleteTarefa(id);
+  return product;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  getByName,
+  newTarefa,
+  updateTarefa,
+  deleteTarefa,
+  duplicate,
+};
