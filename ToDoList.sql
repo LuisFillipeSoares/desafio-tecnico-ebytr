1f4cb4b7feeb6368f98700639e2ac2fbe32586ed
LuisSoares <jobluisfillipe@gmail.com>
2022-06-30 00:01:26 -0300

    [ToDoList SQL] Ajustando banco de dados

--- ToDoList.sql
+++ ToDoList.sql
@@ -1,19 +1,19 @@
-DROP DATABASE IF EXISTS toDoList;
+DROP DATABASE IF EXISTS ToDoList;
 
-CREATE DATABASE toDoList;
+CREATE DATABASE ToDoList;
 
-USE toDoList;
+USE ToDoList;
 
 CREATE TABLE tarefas (
     id INT NOT NULL auto_increment,
     name VARCHAR(30) NOT NULL,
     status VARCHAR(30) NOT NULL DEFAULT "pendente",
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP timezone ,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 ) ENGINE=INNODB;
 
-INSERT INTO toDoList.tarefas (name) VALUES
+INSERT INTO ToDoList.tarefas (name) VALUES
     ("Martelo de Thor"),
     ("Traje de encolhimento"),
     ("Escudo do Capitão América");
