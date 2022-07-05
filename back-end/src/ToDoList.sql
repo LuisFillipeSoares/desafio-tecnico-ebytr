cc4ba4b3a1fa3006a97c3df30c215b9a0658abcb
LuisSoares <jobluisfillipe@gmail.com>
2022-07-05 07:07:43 -0300

    Ajustando o banco de dados

--- back-end/src/ToDoList.sql
+++ back-end/src/ToDoList.sql
@@ -8,12 +8,12 @@ CREATE TABLE tarefas (
     id INT NOT NULL auto_increment,
     name VARCHAR(30) NOT NULL,
     status VARCHAR(30) NOT NULL DEFAULT "pendente",
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP timezone ,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 ) ENGINE=INNODB;
 
 INSERT INTO ToDoList.tarefas (name) VALUES
-    ("Martelo de Thor"),
-    ("Traje de encolhimento"),
-    ("Escudo do Capitão América");
+    ("Teste 1"),
+    ("Teste 2"),
+    ("Teste 3");
